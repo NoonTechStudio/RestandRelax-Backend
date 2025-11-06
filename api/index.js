5343cedc77372ef78285cacd00485eeb882b086a
NZ <noontechstudio@gmail.com>
2025-11-06 10:49:57 +0530

    updated packages

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
-// api/index.js
-import app from '../server.js';
-
-export default app;
+export default async function handler(req, res) {
+  try {
+    const { default: app } = await import('../server.js');
+    return app(req, res);
+  } catch (error) {
+    console.error('Function error:', error);
+    return res.status(500).json({ 
+      error: 'Function failed', 
+      message: error.message,
+      stack: error.stack 
+    });
+  }
+}
